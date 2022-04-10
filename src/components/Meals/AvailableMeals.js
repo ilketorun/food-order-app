d44baad3923581eef86bfbe57f79b768b79df5a0
ilketorun <ilketorun97@gmail.com>
2022-04-10 11:50:20 +0300

    Fetching meals from firebase database Dummy meals are added to the firebase database manually. By using built in Fetch API, data is gathered from the database.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,36 @@
+import { useCallback, useEffect, useState } from "react";
 import Card from "../UI/Card";
 import styles from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  const fetchMeals = useCallback(async () => {
+    const response = await fetch(
+      "https://react-app-2fab7-default-rtdb.firebaseio.com/meals.json"
+    );
+    const responseMeals = await response.json();
+
+    const loadedMeals = [];
+
+    for (const key in responseMeals) {
+      loadedMeals.push({
+        id: key,
+        name: responseMeals[key].name,
+        description: responseMeals[key].description,
+        price: responseMeals[key].price,
+      });
+    }
+    console.log(loadedMeals);
+    setMeals(loadedMeals);
+  }, []);
+
+  useEffect(() => {
+    fetchMeals();
+  }, [fetchMeals]);
+
+  const mealList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
