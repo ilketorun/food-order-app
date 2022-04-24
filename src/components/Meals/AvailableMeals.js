057daffe721406ca92fe1da0fd93520ca1033deb
ilketorun <ilketorun97@gmail.com>
2022-04-24 23:18:02 +0300

    loading feature added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,30 +5,47 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMeals = useCallback(async () => {
-    const response = await fetch(
-      "https://react-app-2fab7-default-rtdb.firebaseio.com/meals.json"
-    );
-    const responseMeals = await response.json();
+    setIsLoading(true);
 
     const loadedMeals = [];
 
-    for (const key in responseMeals) {
-      loadedMeals.push({
-        id: key,
-        name: responseMeals[key].name,
-        description: responseMeals[key].description,
-        price: responseMeals[key].price,
-      });
+    try {
+      const response = await fetch(
+        "https://react-app-2fab7-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseMeals = await response.json();
+
+      for (const key in responseMeals) {
+        loadedMeals.push({
+          id: key,
+          name: responseMeals[key].name,
+          description: responseMeals[key].description,
+          price: responseMeals[key].price,
+        });
+      }
+    } catch (error) {
+      throw new Error(error);
     }
-    console.log(loadedMeals);
+
     setMeals(loadedMeals);
+
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
     fetchMeals();
   }, [fetchMeals]);
+
+  if (isLoading) {
+    return (
+      <section className={styles.loadingMeals}>
+        <h3>Loading...</h3>
+      </section>
+    );
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
